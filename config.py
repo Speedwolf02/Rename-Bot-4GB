5f4513061a61e447685203833b0d62bf7a7ad040
Speedwolf02 <158140629+Speedwolf02@users.noreply.github.com>
2024-05-08 13:21:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+API_ID = int(os.environ.get("API_ID", "21740783"))
+API_HASH = os.environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
+OWNER = int(os.environ.get("OWNER", "6299192020"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Utsxititz_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002005436412")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002134913785"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Speedwolf1:speedwolf24689@cluster0.rgfywsf.mongodb.net/")
+DB_NAME = os.environ.get("DB_NAME", "Speedwolf1")
 
 STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph/file/60cc8a0f42af417fbefdb.jpg")
 
 
 
